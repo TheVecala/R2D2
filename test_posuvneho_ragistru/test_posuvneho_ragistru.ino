d25ccc266a062f0748631cbd67b0d924ddcdebd1
Dušan <the@vecala.cz>
2026-03-22 01:19:15 +0100

    Add 7segment-schema-pinu.jpg and update initialization in setup()

--- test_posuvneho_ragistru/test_posuvneho_ragistru.ino
+++ test_posuvneho_ragistru/test_posuvneho_ragistru.ino
@@ -6,23 +6,18 @@ void setup() {
   pinMode(dataPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
-}
-
-void posliData(byte data) {
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, data);
+    digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b01001111);
   digitalWrite(latchPin, HIGH);
 }
 
-void loop() {
-  // Posun tam
-  for (int i = 0; i < 8; i++) {
-    posliData(1 << i); // To je ten náš trik s posunem jedničky
-    delay(100);
-  }
-  // Posun zpět
-  for (int i = 6; i > 0; i--) {
-    posliData(1 << i);
-    delay(100);
-  }
+void posliData(byte data) {
+
+}
+
+void loop() {     
+ 
+    delay(1000);
+ 
+  
 }
